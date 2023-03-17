62d2cc23a2c143bf6330c172c80c3e754a6178ae
Knives77 <118325569+Knives77@users.noreply.github.com>
2023-03-16 19:34:29 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const men = (id) => {
+    const menu = document.getElementById
+};
+
+const botones = (ifr, url) => {
+    const iframe = document.getElementById(ifr);
+    iframe.setAttribute("src", url);
+};
